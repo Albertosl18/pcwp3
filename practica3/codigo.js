11adfb52c97396ecb5932afaf39af799f51a14a5
Albertosl18 <80276684+Albertosl18@users.noreply.github.com>
2021-05-28 16:50:10 +0200

    Add files via upload

--- practica3/codigo.js
+++ practica3/codigo.js
@@ -1,8 +1,14 @@
 var posicion = 6;
-
+//he utilizado variables globales en vez de session storage antes de darle a jugar(cuando le das si que se usa el session). Para que sea mas facil de probar los mensajes.
+var difi =null;
+var imgsrc =null;
+var contadorM = null;
+var imagenAnt= "";
 function lanzadera() {
     puntuaciones();
     imagenes();
+    sessionStorage['id_imagen2'] = null;
+    sessionStorage['id_dificultad2'] = null;
 }
 
 //index==========================================================================0
@@ -149,7 +155,7 @@ function pedirPuntuacionesA() {
     });
 }
 
-
+/*
 //cargar las imagenes de index
 function imagenes() {
     let url = 'api/imagenes';
@@ -178,7 +184,35 @@ function imagenes() {
         console.log('Fetch Error: ', err);
     });
 }
+*/
 
+function imagenes() {
+    let url = 'api/imagenes';
+    fetch(url).then(function (respuesta) {
+        if (respuesta.ok) {
+            respuesta.json().then(function (datos) {
+                console.log(datos);
+                let ul = document.createElement('ul');
+                var x = 0;
+                datos.FILAS.forEach(function (e, idx, v) {
+                    x++;
+                    let li = document.createElement('li');
+                    li.innerHTML = '<div id="imgen"><img id="img' + x + '" onclick="imagenEle(this.src, this.id);" src="imagenes/' + e.fichero + '" alt="imagen"> <br> ' + e.nombre + '</div>';
+                    ul.appendChild(li);
+                });
+
+                document.querySelector('#imagenesS').innerHTML = ' ';//esto hace que se limpie primero y no se pete de peticiones
+                document.querySelector('#imagenesS').appendChild(ul);//las peticiones aparecen
+            });
+        } else {
+            console.log('Error ' + respuesta.status + ': ' + respuesta.statusText);
+        }
+    }).catch(function (error) {
+        console.log('Fetch Error: ', err);
+    });
+}
+
+/*
 function imagenEle(x, y) {//recibe el src(x) y el id(y)
     sessionStorage['id_imagen']= x;
 
@@ -191,11 +225,29 @@ function imagenEle(x, y) {//recibe el src(x) y el id(y)
     imge.className = 'img';
     sessionStorage['contadorM'] = 1;
 }
+*/
 
+function imagenEle(x, y) {//recibe el src(x) y el id(y)
+    imgsrc= x;
+
+    if(contadorM==1){
+        var imge2 = document.getElementById(imagenAnt);
+        imge2.className = 'img2';
+    }
+    imagenAnt = y;
+    var imge = document.getElementById(y);
+    imge.className = 'img';
+    contadorM = 1;
+}
+/*
 function dificultadEle(y){
     sessionStorage['id_dificultad'] = y;
 }
-
+*/
+function dificultadEle(y) {
+    difi = y;
+}
+/*
 function jugar(){
     if (sessionStorage['id_imagen'] == null && sessionStorage['id_dificultad'] == null){
         console.log("ninguno");
@@ -212,6 +264,26 @@ function jugar(){
         window.location.href ="juego.html";
     }else{
         console.log("falta introducir algun dato")
+    }
+}
+*/
+
+function jugar() {
+    if (imgsrc == null && difi == null) {
+        console.log("ninguno");
+        mostrarMensajeNingunaOP();
+    } else if (imgsrc == null && difi != null) {
+        console.log("falta imagen");
+        mostrarMensajeNoImagen();
+    } else if (imgsrc != null && difi == null) {
+        console.log("falta dificultad");
+        mostrarMensajeNoDificultad();
+    } else if (imgsrc != null && difi != null) {
+        sessionStorage['id_imagen2'] = imgsrc;
+        sessionStorage['id_dificultad2'] = difi;
+        window.location.href = "juego.html";
+    } else {
+        console.log("Error?")
     }
 }
 
@@ -270,22 +342,39 @@ function mostrarMensajeNoDificultad() {
 
 //juego=================================================================0
 function cargarImagen() {
-    let cv1 = document.querySelector('#iFinal'),
-        cv2 = document.querySelector('#cv01'),
-        ctx2 = cv2.getContext('2d');
-       // factor;
-    //factor = cv2.width / cv1.width;
-    ctx2.drawImage(cv1, 0, 0, cv2.width, cv2.height);
+    let img = new Image();
+
+    img.onload = function () {
+        let cv = document.querySelector('#cv01'),
+            ctx = cv.getContext('2d'),
+            factor = cv.width / img.width,
+            posY = (cv.height - img.height * factor) / 2;
+
+        ctx.drawImage(img, 0, posY, cv.width, img.height * factor);
+    };
+
+    img.src = sessionStorage['id_imagen2'];
 }
 
 function lanzaderaj() {
-    document.getElementById("iFinal").src=sessionStorage['id_imagen'];
-    cargarImagen();
-    divisiones();
+    if (sessionStorage['id_imagen2'] == null || sessionStorage['id_dificultad2']==null){
+        window.location.href = "index.html";
+    }else{
+        divisiones();
+    }
 }
-
 function prepararCanvas() {
     let cv = document.querySelector('#cv01');
+
+    cv.width = 480;
+    cv.height = 360;
+
+    cargarImagen();
+}
+
+function prepararCanvas2() {
+    copiarImagen();
+    let cv = document.querySelector('#cv02');
 
     cv.width = 480;
     cv.height = 360;
@@ -295,8 +384,8 @@ function prepararCanvas() {
             y = evt.offsetY,
             ancho,
             alto,
-            fila = Math.floor(y / alto),
-            columna = Math.floor(x / ancho);
+            fila,
+            columna;
         if (sessionStorage['id_dificultad2'] == 1) {
             ancho = cv.width / 4;
             alto = cv.height / 4;
@@ -307,11 +396,13 @@ function prepararCanvas() {
             ancho = cv.width / 8;
             alto = cv.height / 8;
         }
+        fila = Math.floor(y / alto);
+        columna = Math.floor(x / ancho);
         console.log(x + ',' + y);
         console.log(columna + ',' + fila);//con esto saco la region de las divisiones en la que estoy
 
         //como pintar la region correspondiente
-        let cv1 = document.querySelector('#iFinal'),
+        let cv1 = document.querySelector('#cv01'),
             cv2 = cv,
             ctx2 = cv2.getContext('2d');
 
@@ -319,9 +410,18 @@ function prepararCanvas() {
     };
 }
 
+function copiarImagen() {
+    let cv1 = document.querySelector('#cv01'),
+        ctx1 = cv1.getContext('2d'),
+        cv2 = document.querySelector('#cv02'),
+        ctx2 = cv2.getContext('2d');
+
+    ctx2.drawImage(cv1, 0, 0);
+}
+
 function divisiones() {
     if (sessionStorage['id_dificultad2']==1){
-        let cv2 = document.querySelector('#cv01'),
+        let cv2 = document.querySelector('#cv02'),
             ctx2 = cv2.getContext('2d'),
             ancho = cv2.width / 4,
             alto = cv2.height / 4;
@@ -340,7 +440,7 @@ function divisiones() {
         }
         ctx2.stroke();
     } else if (sessionStorage['id_dificultad2'] == 2) {
-        let cv2 = document.querySelector('#cv01'),
+        let cv2 = document.querySelector('#cv02'),
             ctx2 = cv2.getContext('2d'),
             ancho = cv2.width / 6,
             alto = cv2.height / 6;
@@ -359,7 +459,7 @@ function divisiones() {
         }
         ctx2.stroke();
     } else if (sessionStorage['id_dificultad2'] == 3) {
-        let cv2 = document.querySelector('#cv01'),
+        let cv2 = document.querySelector('#cv02'),
             ctx2 = cv2.getContext('2d'),
             ancho = cv2.width / 8,
             alto = cv2.height / 8;
